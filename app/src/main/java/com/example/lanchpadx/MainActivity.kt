c1ed632c6e1bfd59e709be4208cbd04ca00afc23
Andrii <dyedfox.ra@gmail.com>
2021-12-25 19:33:37 +0100

    init commit

---
+++ app/src/main/java/com/example/lanchpadx/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.lanchpadx
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lanchpadx.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
