439e39f1080bbfce127e93db7b6ea3b4732d741a
Andrii <dyedfox.ra@gmail.com>
2021-12-26 13:22:11 +0100

    add correct service to launchpad provider

--- app/src/main/java/com/example/launchpadx/domain/interaction/launchpads/LaunchpadProviderImpl.kt
+++ app/src/main/java/com/example/launchpadx/domain/interaction/launchpads/LaunchpadProviderImpl.kt
@@ -1,13 +1,14 @@
 package com.example.launchpadx.domain.interaction.launchpads
 
+import com.example.launchpadx.data.api.service.launchpads.LaunchpadsService
 import com.example.launchpadx.data.entity.Launchpad
 import retrofit2.Response
 
 class LaunchpadProviderImpl(
-    private val launchpadProvider: LaunchpadProvider
+    private val launchpadsService: LaunchpadsService
 ) : LaunchpadProvider {
 
     override suspend fun execute(siteId: String): Response<Launchpad> {
-        return launchpadProvider.execute(siteId)
+        return launchpadsService.getOneLaunchpad(siteId)
     }
 }
